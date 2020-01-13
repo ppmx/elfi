5a814aa3574dcc43ae71d07cca259ad2531a3908
mx <ppmx@users.noreply.github.com>
2020-01-13 21:11:42 +0100

    add hello world that is "optimized" for shellcode

---
+++ shellcode/hello_world.c
@@ -0,0 +1,33 @@
+#define STRLEN(s) (sizeof(s)/sizeof(s[0]))
+
+inline int write(int, const void *, int) __attribute__((always_inline));
+
+int write(int fildes, const void *buf, int nbyte)
+{
+    int ret;
+
+    asm("mov rdi, %0" : : "r"((long) fildes));
+    asm("mov rsi, %0" : : "r"(buf));
+    asm("mov rdx, %0" : : "r"((long) nbyte));
+
+    asm("mov rax, 1");
+    asm("syscall");
+
+    asm("mov %0, eax" : "=r" (ret));
+
+    return ret;
+}
+
+void _start()
+{
+    char s[] = "Hello world.\n";
+
+    write(1, s, STRLEN(s));
+
+    // exit(0):
+    for (;;) {
+        asm("xor rdi, rdi");
+        asm("mov rax, 60");
+        asm("syscall");
+    }
+}
