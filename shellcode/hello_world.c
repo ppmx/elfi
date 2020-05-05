34971d318830af39bee93cc1687d52340ffc0c15
mx <ppmx@users.noreply.github.com>
2020-05-05 18:57:12 +0200

    adjust indenting

--- shellcode/hello_world.c
+++ shellcode/hello_world.c
@@ -4,30 +4,30 @@ inline int write(int, const void *, int) __attribute__((always_inline));
 
 int write(int fildes, const void *buf, int nbyte)
 {
-    int ret;
+	int ret;
 
-    asm("mov rdi, %0" : : "r"((long) fildes));
-    asm("mov rsi, %0" : : "r"(buf));
-    asm("mov rdx, %0" : : "r"((long) nbyte));
+	asm("mov rdi, %0" : : "r"((long) fildes));
+	asm("mov rsi, %0" : : "r"(buf));
+	asm("mov rdx, %0" : : "r"((long) nbyte));
 
-    asm("mov rax, 1");
-    asm("syscall");
+	asm("mov rax, 1");
+	asm("syscall");
 
-    asm("mov %0, eax" : "=r" (ret));
+	asm("mov %0, eax" : "=r" (ret));
 
-    return ret;
+	return ret;
 }
 
 void _start()
 {
-    char s[] = "Hello world.\n";
+	char s[] = "Hello world.\n";
 
-    write(1, s, STRLEN(s));
+	write(1, s, STRLEN(s));
 
-    // exit(0):
-    for (;;) {
-        asm("xor rdi, rdi");
-        asm("mov rax, 60");
-        asm("syscall");
-    }
+	// exit(0):
+	for (;;) {
+		asm("xor rdi, rdi");
+		asm("mov rax, 60");
+		asm("syscall");
+	}
 }
